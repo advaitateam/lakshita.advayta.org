eb6619e546b5f1e2eedda8db0700da7cb309751f
Vlad <vladka@webdevelop.pro>
2024-08-26 22:19:38 +0200

    initial commit

---
+++ make.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+case $1 in
+
+run-dev)
+  hugo serve
+  ;;
+
+install)
+  mkdir -p dist/
+  ;;
+
+lint)
+  echo 'todo'
+  ;;
+
+build)
+  BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+  hugo build --mode $BRANCH_NAME
+  ;;
+
+deploy-dev)
+  BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+  SHORT_SHA=`git rev-parse --short HEAD`
+  echo $BRANCH_NAME, $SHORT_SHA
+  SHORT_SHA=$SHORT_SHA BRANCH_NAME=$BRANCH_NAME yarn build --mode dev
+  cp -rf public/* dist/
+  # yarn add --global wrangler
+  wrangler pages deploy dist --project-name="lakshita-advayta-org" --commit-dirty true --branch main --commit-hash $SHORT_SHA --commit-message $SHORT_SHA
+  ;;
+
+deploy-master)
+  BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+  SHORT_SHA=`git rev-parse --short HEAD`
+  echo $BRANCH_NAME, $SHORT_SHA
+  SHORT_SHA=$SHORT_SHA BRANCH_NAME=$BRANCH_NAME yarn build --mode production
+  cp -rf public/* dist/
+  # yarn add --global wrangler
+  wrangler pages deploy dist --project-name="webdevelop-pro" --commit-dirty true --branch main --commit-hash $SHORT_SHA --commit-message $SHORT_SHA
+  ;;
+
+help)
+  cat make.sh | grep "^[a-z-]*)"
+  ;;
+
+*)
+  echo "unknown $1, try help"
+  ;;
+
+esac
